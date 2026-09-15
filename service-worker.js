58531e4938b57648a5f40393cc30656c5b18d466
dawnel7 <dawnel7@yahoo.com>
2026-09-14 17:48:12 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,5 @@
-const CACHE="back-to-yourself-v32";
+const CACHE="back-to-yourself-v33";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.png","./brand-logo.png"];
-const CORE=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  const url=new URL(e.request.url);
-  const isCore=CORE.some(path=>new URL(path,self.location.href).pathname===url.pathname);
-  if(isCore){
-    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
-    return;
-  }
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match("./index.html"))));
-});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match("./index.html"))))});
